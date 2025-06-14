29eafccc25ed46b8636715b31dad8b2c6aacd9af
seungheonlee <lsh2613@naver.com>
2025-06-14 14:19:42 +0900

    first commit

---
+++ src/main/java/com/pitchaintranscoding/PitchainTranscodingApplication.java
@@ -0,0 +1,13 @@
+package com.pitchaintranscoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitchainTranscodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PitchainTranscodingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pitchaintranscoding/PitchainTranscodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pitchaintranscoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitchainTranscodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
